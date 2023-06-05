ab5d0861594d915d517625c4b660c242ed308a04
Vittorio Shiffman <100438794+vittorio-sh@users.noreply.github.com>
2023-06-05 11:42:19 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,183 +1,208 @@
 import pygame
 import random
+import sys
 
-#Variables used to make colours 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 128, 0)
-RED = (255, 0, 0)
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
 
-pygame.init()
+CARD_WIDTH = 121
+CARD_HEIGHT = 188
 
-# check if game started
-game_started = False
+SUITS = ['H', 'D', 'C', 'S']
+RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+#using dictionary to assign a color to each card based on suit this helps with step iv)
+COLORS = {'H': 'red', 'D': 'red', 'C': 'black', 'S': 'black'}
 
-# Set the width and height of the screen
-screen_width = 1280
-screen_height = 720
-screen = pygame.display.set_mode([screen_width, screen_height])
-pygame.display.set_caption("Solitaire")
+class Card:
+    def __init__(self, rank, suit, face_up=False):
+        self.rank = rank  # (iii) Card values are appropriately displayed
+        self.suit = suit  # (ii) Card suits are appropriately displayed
+        self.color = COLORS[suit]
+        self.face_up = face_up
+        self.face_up_image = pygame.image.load(f"assets/front_card/{rank}{suit}.png")  # (i) Front side of the card is displayed
+        self.face_down_image = pygame.image.load("assets/back_card/back.png")  # (i) Back side of the card is displayed
+        self.image = self.face_down_image if not face_up else self.face_up_image
+        self.image = pygame.transform.scale(self.image, (CARD_WIDTH, CARD_HEIGHT))
 
-# Define card dimensions note the images I downloaded are too big so the screen is also set pretty large. We can download differents PNG later
-card_width = 121
-card_height = 188
+    def flip(self):
+        self.face_up = not self.face_up
+        self.image = self.face_up_image if self.face_up else self.face_down_image
+        self.image = pygame.transform.scale(self.image, (CARD_WIDTH, CARD_HEIGHT))
 
-# Load the card images
-class CardImage(pygame.Surface):
-    def __init__(self, size, *args, **kwargs):
-        super().__init__(size, *args, **kwargs)
-        self.rect = self.get_rect()
+#following classes are used to ensure game logic with adding and transfering cards
+class Pile:
+    def __init__(self):
+        self.cards = []
 
-card_images = {}
-suits = ['H', 'D', 'C', 'S']
-ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-
-#for loop to assign rank and suit to each PNG image. Don't change the PNG names are this wont work
-for suit in suits:
-    for rank in ranks:
-        card_name = f"{rank}{suit}"
-        image_path = f"assets/front_card/{card_name}.png"
-        card_image = pygame.image.load(image_path)
-        card_image = pygame.transform.scale(card_image, (card_width, card_height))
-        card_images[card_name] = CardImage((card_width, card_height))
-        card_images[card_name].blit(card_image, (0, 0))
-
-# Load the card back image
-card_back_image = pygame.image.load("assets/back_card/back.png")
-card_back_image = pygame.transform.scale(card_back_image, (card_width, card_height))
-
-# Load the foundation image
-foundation_image = pygame.image.load("assets/extra/foundation.png")
-foundation_image = pygame.transform.scale(foundation_image, (card_width, card_height))
-
-# Load the waste image
-waste_image = pygame.image.load("assets/extra/waste.png")
-waste_image = pygame.transform.scale(waste_image, (card_width, card_height))
-
-# Define pile dimensions
-pile_width = card_width + 20
-pile_height = card_height + 20
-
-# Calculate the positions for foundation piles and deck
-foundation_start_x = 20
-foundation_start_y = 20
-foundation_spacing = pile_width
-deck_x = screen_width - pile_width - 20
-deck_y = 20
-
-# Create rectangle plots for piles
-tableau_rects = [[] for _ in range(7)]
-foundation_rects = [pygame.Rect(foundation_start_x + i * foundation_spacing, foundation_start_y, card_width, card_height) for i in range(4)]
-waste_rect = pygame.Rect(foundation_start_x + 4 * foundation_spacing + 20, foundation_start_y, card_width, card_height)
-deck_rect = pygame.Rect(deck_x, deck_y, card_width, card_height)
-
-# animation speed
-deal_speed = 5  # Speed of dealer
-deal_delay = 10  # Delay between dealing each card
-
-# Function to shuffle and deal cards
-def shuffle_and_deal():
-    # Shuffle the deck
-    deck = list(card_images.keys())
-    random.shuffle(deck)
-
-    # For loop to deal cards to each tableaux
-    for i in range(7):
-        for j in range(i + 1):
-            if not deck:
-                break  # Break if there are no more cards in the deck
-            card = deck.pop()
-            card_rect = card_images[card].get_rect(center=waste_rect.center)  # Start from waste pile
-            card_images[card].rect = card_rect
-            animate_card_deal(card_images[card], i, deal_delay * (i + j))
-            tableau_rects[i].append(card)  # Add the card to the tableau pile
+    def add_card(self, card):
+        self.cards.append(card)  # (iii) As cards are sorted into their piles, they must be subtracted from the full deck of cards.
 
 
+class Tableau(Pile):
+    def can_add(self, card):
+        if not self.cards:  
+            return card.rank == 'K'  # (v) Spare tableau spots can only be filled with kings
+        else:
+            top_card = self.cards[-1]
+            return COLORS[card.suit] != COLORS[top_card.suit] and RANKS.index(card.rank) == RANKS.index(top_card.rank) - 1  # (iv) Tableau cards can only be stacked in alternating colors
 
-# Function to animate card dealing. I followed a youtube video and googled please don't mess with it because it's hard to fix..
-def animate_card_deal(card_image, tableau, delay):
-    if tableau_rects[tableau]:
-        last_card = card_images[tableau_rects[tableau][-1]]
-        target_pos = (last_card.rect.centerx, last_card.rect.centery + 20)
-    else:
-        target_pos = (pile_width * tableau + 20, pile_height + 40)
 
-    dx = (target_pos[0] - card_image.rect.centerx) / deal_speed
-    dy = (target_pos[1] - card_image.rect.centery) / deal_speed
+class Foundation(Pile):
+    def can_add(self, card):
+        if not self.cards:
+            return card.rank == 'A'  # (vi) Foundations can only be filled starting with an ace
+        else:
+            top_card = self.cards[-1]
+            return card.suit == top_card.suit and RANKS.index(card.rank) == RANKS.index(top_card.rank) + 1  # (i) Rank of cards is functional
 
-    pygame.time.delay(delay)
-    for _ in range(deal_speed):
-        card_image.rect.move_ip(dx, dy)
-        screen.fill(GREEN)
-        draw_tableau_piles()
-        draw_foundation_piles()
-        pygame.draw.rect(screen, WHITE, waste_rect)
-        screen.blit(card_image, card_image.rect)
-        pygame.display.flip()
-        pygame.time.delay(10)  # Adjust animation speed
 
-# Function to draw tableau piles
-def draw_tableau_piles():
-    for i, pile in enumerate(tableau_rects):
-        x = 20 + i * pile_width
-        y = 20 + card_height  # Adjust the starting y position
-        for j, card in enumerate(pile):
-            if j >= len(pile) - 1 or not game_started:
-                screen.blit(card_images[card], (x, y))
+class Waste(Pile):
+    #need to add logic to pop from deck and add card to waste pile upon clicking
+    pass
+
+
+class Deck:
+    def __init__(self):
+        #declaring deck of cards and setting the image plus scalling
+        self.cards = []
+        self.deck_image = pygame.image.load("assets/back_card/back.png")  # (i) Back side of the card is displayed
+        self.deck_image = pygame.transform.scale(self.deck_image, (CARD_WIDTH, CARD_HEIGHT))
+
+    def fill(self):
+        #filling deck with cards by using a for loop
+        for suit in SUITS:
+            for rank in RANKS:
+                card = Card(rank, suit)
+                self.cards.append(card)  # (ii) A full deck of cards is implemented.
+
+    def shuffle(self):
+        random.shuffle(self.cards)  # (ii) Deck can be shuffled
+
+    def deal(self, tableau_piles):
+        #ensure the right amount of cards are placed per tableau
+        for i in range(7):
+            for j in range(i + 1):
+                card = self.cards.pop()  # (iii) cards are sorted into their piles, than subtracted from full deck
+                tableau_pile = tableau_piles[i]
+                tableau_pile.add_card(card)
+                if j == i:  
+                    card.flip()
+
+
+class GameInterface:
+    def __init__(self, game):
+        self.game = game
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Solitaire")
+        self.screen.fill((0, 128, 0))
+
+    def draw_start_button(self):
+        #drawring start button i need to add functionality to potentially switch it to a restart button after clicked
+        button_width = 100
+        button_height = 50
+        button_color = (200, 200, 200)
+        button_text = "Start"
+
+        button_rect = pygame.Rect(10, 10, button_width, button_height)
+        pygame.draw.rect(self.screen, button_color, button_rect)
+
+        font = pygame.font.SysFont(None, 24)
+        text_surface = font.render(button_text, True, (0, 0, 0))
+        text_rect = text_surface.get_rect(center=button_rect.center)
+        self.screen.blit(text_surface, text_rect)
+
+    def draw_tableau_piles(self):
+        pile_x = 100
+        pile_y = 300
+        pile_spacing = 20
+
+        for tableau_pile in self.game.tableau_piles:
+            #the deal function will ensure the right amount is placed on each tableau this for loop is just used to draw and space
+            for i, card in enumerate(tableau_pile.cards):
+                card_x = pile_x
+                card_y = pile_y + i * pile_spacing
+                self.screen.blit(card.image, (card_x, card_y))
+
+            pile_x += CARD_WIDTH + pile_spacing
+
+    def draw_deck_and_waste_piles(self):
+        #the deck is drawn on the top left of the screen but need to add space for cards when clicked
+        deck_x = 100
+        deck_y = 50
+        self.screen.blit(self.game.deck.deck_image, (deck_x, deck_y))
+
+        if self.game.waste_pile.cards:
+            self.screen.blit(self.game.waste_pile.cards[-1].image, (deck_x + CARD_WIDTH + 20, deck_y))
+
+    def draw_foundation_piles(self):
+        foundation_x = WINDOW_WIDTH - 100 - 4 * CARD_WIDTH
+        foundation_y = 20
+
+        for foundation_pile in self.game.foundation_piles:
+            #this will draw the foundation piles based on whether theres a card there or empty 
+            if foundation_pile.cards:
+                #drawing card
+                self.screen.blit(foundation_pile.cards[-1].image, (foundation_x, foundation_y))
             else:
-                screen.blit(card_back_image, (x, y))  # Display the back image of the card
-            y += 20  # Increase the y position
+                #drawing rectangle
+                pygame.draw.rect(self.screen, (255, 255, 255), (foundation_x, foundation_y, CARD_WIDTH, CARD_HEIGHT), 1)
 
-# Function to draw foundation piles
-def draw_foundation_piles():
-    for i, rect in enumerate(foundation_rects):
-        screen.blit(foundation_image, rect)  # Draw foundation image
-        if i >= 1 or not game_started:
-            pygame.draw.rect(screen, WHITE, rect, 1)  # Draw foundation rectangle border
+            foundation_x += CARD_WIDTH + 20
 
-# Function to handle button click
-def handle_button_click(pos):
-    global game_started
-    if not game_started:
-        game_started = True
-        shuffle_and_deal()
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-# Game loop
-done = False
-clock = pygame.time.Clock()
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                mouse_pos = pygame.mouse.get_pos()
+                button_rect = pygame.Rect(10, 10, 100, 50)
 
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            pos = pygame.mouse.get_pos()
-            handle_button_click(pos)
+                if button_rect.collidepoint(mouse_pos):
+                    self.game.start_button_clicked = True
 
-    screen.fill(GREEN)
+    def render(self):
+        self.screen.fill((0, 128, 0))
+        self.draw_tableau_piles()
+        self.draw_foundation_piles()
 
-    # Draw tableau piles
-    draw_tableau_piles()
+        if self.game.start_button_clicked:
+            self.draw_deck_and_waste_piles()
 
-    # Draw foundation piles
-    draw_foundation_piles()
+        self.draw_start_button()
+        pygame.display.update()
 
-    # Draw waste pile
-    screen.blit(waste_image, waste_rect)
 
-    # Draw deck
-    pygame.draw.rect(screen, WHITE, deck_rect)
+class Game:
+    def __init__(self):
+        self.tableau_piles = [Tableau() for _ in range(7)]
+        self.foundation_piles = [Foundation() for _ in range(4)]
+        self.waste_pile = Waste()
+        self.deck = Deck()
+        self.setup()
+        self.start_button_clicked = False
+        self.cards_dealt = False
 
-    # Draw start button
-    button_rect = pygame.Rect(300, 540, 100, 50)
-    pygame.draw.rect(screen, WHITE, button_rect)
-    font = pygame.font.Font(None, 24)
-    if not game_started:
-        text = font.render("Start", True, BLACK)
-        text_rect = text.get_rect(center=button_rect.center)
-        screen.blit(text, text_rect)
+    def setup(self):
+        self.deck.fill()  # (ii) A full deck of cards is implemented.
+        self.deck.shuffle()  # (ii) Deck can be shuffled
 
-    pygame.display.flip()
-    clock.tick(60)
+    def run(self):
+        pygame.init()
+        interface = GameInterface(self)
+        while True:
+            interface.events()
+            if self.start_button_clicked and not self.cards_dealt:
+                self.deck.deal(self.tableau_piles) 
+                self.cards_dealt = True
+            interface.render()
 
-pygame.quit()
+
+def main():
+    game = Game()
+    game.run()
+
+
+if __name__ == "__main__":
+    main()
